85d344d98c55f0d3111fcaad4e9387213ba6fd39
Adeline Non-Binary <140004151+AdenineBliss@users.noreply.github.com>
2025-04-16 12:06:11 -0500

    Update ThoughtWorker_AccostaPop.cs
    fixed

--- Source/ThoughtWorker_AccostaPop.cs
+++ Source/ThoughtWorker_AccostaPop.cs
@@ -7,39 +7,31 @@ namespace AddieStorytellers
     {
         protected override ThoughtState CurrentStateInternal(Pawn p)
         {
-            Log.Message("Suffer two");
-            if (!p.IsColonist)
+            if (!p.IsColonist || p.MapHeld == null) return ThoughtState.Inactive;
+
+            if (Current.Game?.storyteller?.def?.defName == DefOfs.Addie_AccostaAntagonist.defName)
             {
-                return false;
-                return ThoughtState.Inactive;
-            }
-            
-            return true;
-            return ThoughtState.Inactive;
-            /*
-            if (ThoughtUtility.ThoughtNullified(p, def))
-            {
-                return ThoughtState.Inactive;
-            }
-            int freeColonistsAndPrisonersSpawnedCount = p.Map.mapPawns.FreeColonistsAndPrisonersSpawnedCount;
-            if (freeColonistsAndPrisonersSpawnedCount <= 1)
-            {
-                return ThoughtState.ActiveAtStage(0);
-            }
-            if (freeColonistsAndPrisonersSpawnedCount <= 3)
-            {
-                return ThoughtState.ActiveAtStage(1);
-            }
-            if (freeColonistsAndPrisonersSpawnedCount <= 7)
-            {
-                return ThoughtState.ActiveAtStage(2);
-            }
-            if (freeColonistsAndPrisonersSpawnedCount <= 15)
-            {
+                int freeColonistsAndPrisonersSpawnedCount = p.Map.mapPawns.FreeColonistsAndPrisonersSpawnedCount;
+                if (freeColonistsAndPrisonersSpawnedCount <= 1)
+                {
+                    return ThoughtState.ActiveAtStage(0);
+                }
+                if (freeColonistsAndPrisonersSpawnedCount <= 3)
+                {
+                    return ThoughtState.ActiveAtStage(1);
+                }
+                if (freeColonistsAndPrisonersSpawnedCount <= 7)
+                {
+                    return ThoughtState.ActiveAtStage(2);
+                }
+                if (freeColonistsAndPrisonersSpawnedCount <= 15)
+                {
+                    return ThoughtState.ActiveAtStage(3);
+                }
                 return ThoughtState.ActiveAtStage(3);
             }
-            return ThoughtState.ActiveAtStage(3);
-            */
+            return ThoughtState.Inactive;
+
         }
     }
 }
